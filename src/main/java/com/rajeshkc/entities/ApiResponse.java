01a325fe2d542ee2e6116ee1e071c4e72b8317f3
Rajesh-K-C <rajeshkc1111.rk@gmail.com>
2025-12-25 22:24:53 +0545

    Use lombok annotations in ApiResponse class

--- src/main/java/com/rajeshkc/entities/ApiResponse.java
+++ src/main/java/com/rajeshkc/entities/ApiResponse.java
@@ -1,38 +1,12 @@
 package com.rajeshkc.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ApiResponse<T> {
-    private String message;
     private boolean success;
+    private String message;
     private T data;
-
-    public ApiResponse(boolean success, String message, T data) {
-        this.message = message;
-        this.success = success;
-        this.data = data;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public boolean isSuccess() {
-        return success;
-    }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
-
 }
